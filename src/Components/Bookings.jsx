9e71533be8730dd6797a58823c979a5949b604fe
diegomdomi <diegomdomi@gmail.com>
2023-02-24 10:28:53 +0100

    changes in bookings

--- src/Components/Bookings.jsx
+++ src/Components/Bookings.jsx
@@ -7,6 +7,110 @@ import { fetchAllBookings } from '../Redux/bookingsSlice.js';
 import { getSingleBooking } from '../Redux/bookingsSlice.js';
 import { Spinner } from './Spinner';
 
+
+const Bookings = () => {
+
+  const bookingstore = useSelector(state=>state.bookingsSlice.list)
+  const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(fetchAllBookings())
+    }, [dispatch,bookingstore])
+    
+    const handleClick = (id)=>{
+      dispatch(getSingleBooking(id))
+      console.log(id);
+    }
+  return (
+    <>
+      {<TableContainer>
+        <HeaderContainer>
+          <ListTitleTopContainer>
+            <ListTitleTop>All Guest</ListTitleTop>
+            <ListTitleTop>Pending</ListTitleTop>
+            <ListTitleTop>Booked</ListTitleTop>
+            <ListTitleTop>Canceled</ListTitleTop>
+            <ListTitleTop>Refund</ListTitleTop>
+          </ListTitleTopContainer>
+          <NewRoomButtonContainer>
+            <NewRoomButton bgColor={'#135846'}
+              color={"white"}>1 November - 30 November 2020</NewRoomButton>
+            <NewRoomButton color={'#135846'}>Newest</NewRoomButton>
+          </NewRoomButtonContainer>
+        </HeaderContainer>
+        <Table >
+          <thead>
+            <RowHeader>
+              <TableHead>Guest</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Check In</TableHead>
+              <TableHead>Ckeck out</TableHead>
+              <TableHead>Special Request</TableHead>
+              <TableHead>Room Type</TableHead>
+              <TableHead>Status</TableHead>
+            </RowHeader>
+          </thead>
+          <tbody>
+      { bookingstore.map((guest) => 
+        (<TableRow >
+          <ImgContainer>
+            <TableData onClick={() =>handleClick(guest.id)}>
+                <Link to={`/bookings/${guest.id}`} >
+                  <img src={guest.img} alt="img"/> 
+                </Link>
+            </TableData>
+              <ParagraphContainer>
+                <Paragraph>{guest.Guest}</Paragraph>
+                <Span color={'#799283'}>#000123456</Span>
+              </ParagraphContainer>
+              </ImgContainer> 
+            <TableData>
+              <Paragraph>{guest.Order_Date}</Paragraph>
+            </TableData> 
+            <TableData>
+              <Paragraph>
+                {guest.Check_in}
+              </Paragraph>
+                <Span>9:46 PM</Span>
+            </TableData> 
+            <TableData>
+              <Paragraph>
+              {guest.Check_out}
+              </Paragraph>
+              <Span>6:12 PM</Span>
+            </TableData> 
+            <TableData>
+              <ViewNotesButton 
+                color={guest.Status === 'Refund' ? '#212121' : '#799283'}
+                bgcolor={guest.Status === 'Refund'? '#EEF9F2' : 'white'}
+                border={guest.Status === 'Booked' ? '1px solid ' : 'none'}>
+                {guest.Special_Request}
+              </ViewNotesButton>
+            </TableData> 
+            <TableData>
+              <Paragraph>
+              {guest.Room_type}
+              </Paragraph>
+            </TableData> 
+            <TableData>
+              <Button 
+                color={guest.Status === 'Refund'? 'red' : '#5AD07A'}
+                bgcolor={guest.Status === 'Refund'? '#FFEDEC' : '#E8FFEE'}>
+                {guest.Status}
+              </Button>
+            </TableData> 
+          </TableRow> ))
+      }
+    </tbody>
+   </Table>
+  </TableContainer>}
+    </>
+  )
+}
+
+export default Bookings
+
+
 const TableContainer = styled.div`
   width:90%;
   margin: 0 auto;
@@ -127,104 +231,3 @@ const NewRoomButton = styled.button`
   padding: 13px 23px;
   border:none;
 `
-const Bookings = () => {
-
-  const bookingstore = useSelector(state=>state.bookingsSlice.list)
-  const dispatch = useDispatch();
-
-    useEffect(() => {
-      dispatch(fetchAllBookings())
-    }, [dispatch,bookingstore])
-    
-    const handleClick = (id)=>{
-      dispatch(getSingleBooking(id))
-      console.log(id);
-    }
-  return (
-    <>
-      <TableContainer>
-        <HeaderContainer>
-          <ListTitleTopContainer>
-            <ListTitleTop>All Guest</ListTitleTop>
-            <ListTitleTop>Pending</ListTitleTop>
-            <ListTitleTop>Booked</ListTitleTop>
-            <ListTitleTop>Canceled</ListTitleTop>
-            <ListTitleTop>Refund</ListTitleTop>
-          </ListTitleTopContainer>
-          <NewRoomButtonContainer>
-            <NewRoomButton bgColor={'#135846'}
-              color={"white"}>1 November - 30 November 2020</NewRoomButton>
-            <NewRoomButton color={'#135846'}>Newest</NewRoomButton>
-          </NewRoomButtonContainer>
-        </HeaderContainer>
-        <Table >
-          <thead>
-            <RowHeader>
-              <TableHead>Guest</TableHead>
-              <TableHead>Order Date</TableHead>
-              <TableHead>Check In</TableHead>
-              <TableHead>Ckeck out</TableHead>
-              <TableHead>Special Request</TableHead>
-              <TableHead>Room Type</TableHead>
-              <TableHead>Status</TableHead>
-            </RowHeader>
-          </thead>
-          <tbody>
-      { bookingstore.map((guest) => 
-        (<TableRow >
-          <ImgContainer>
-            <TableData onClick={() =>handleClick(guest.id)}>
-                <Link to={`/bookings/${guest.id}`} >
-                  <img src={guest.img} alt="img"/> 
-                </Link>
-            </TableData>
-              <ParagraphContainer>
-                <Paragraph>{guest.Guest}</Paragraph>
-                <Span color={'#799283'}>#000123456</Span>
-              </ParagraphContainer>
-              </ImgContainer> 
-            <TableData>
-              <Paragraph>{guest.Order_Date}</Paragraph>
-            </TableData> 
-            <TableData>
-              <Paragraph>
-                {guest.Check_in}
-              </Paragraph>
-                <Span>9:46 PM</Span>
-            </TableData> 
-            <TableData>
-              <Paragraph>
-              {guest.Check_out}
-              </Paragraph>
-              <Span>6:12 PM</Span>
-            </TableData> 
-            <TableData>
-              <ViewNotesButton 
-                color={guest.Status === 'Refund' ? '#212121' : '#799283'}
-                bgcolor={guest.Status === 'Refund'? '#EEF9F2' : 'white'}
-                border={guest.Status === 'Booked' ? '1px solid ' : 'none'}>
-                {guest.Special_Request}
-              </ViewNotesButton>
-            </TableData> 
-            <TableData>
-              <Paragraph>
-              {guest.Room_type}
-              </Paragraph>
-            </TableData> 
-            <TableData>
-              <Button 
-                color={guest.Status === 'Refund'? 'red' : '#5AD07A'}
-                bgcolor={guest.Status === 'Refund'? '#FFEDEC' : '#E8FFEE'}>
-                {guest.Status}
-              </Button>
-            </TableData> 
-          </TableRow> ))
-      }
-    </tbody>
-  </Table>
-  </TableContainer>
-    </>
-  )
-}
-
-export default Bookings
