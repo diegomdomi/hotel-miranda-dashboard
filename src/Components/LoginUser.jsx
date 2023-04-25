d5b27b24f99bc540543a1e70f8f0a867307413b7
diegomdomi <diegomdomi@gmail.com>
2023-04-25 11:04:50 +0200

    changes in new branch to deploy

--- src/Components/LoginUser.jsx
+++ src/Components/LoginUser.jsx
@@ -1,3 +1,4 @@
+/*Login*/
 import React , { useState} from 'react';
 import styled from "styled-components";
 import Button from "./ButtonStyled";
@@ -5,11 +6,29 @@ import Input from "./Input";
 import {  useNavigate } from 'react-router-dom'
 import { useAuth } from '../Context/AuthProvider';
 import { ACTIONS_USER } from '../Context/actions';
+import { ToastContainer, toast } from 'react-toastify';
 
+const url = process.env.REACT_APP_URL
 
+const sendForm = async (email,password) => {
+  try{
+    const requestOptions = {
+      method: 'POST',
+      mode: "cors",
+      Accept: 'application/json',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "email": email , "password": password})
+  };
+  const response = await fetch(`${url}/login`, requestOptions);
+  const data = await response.json();
+  return data
+    }
+    catch (err) {
+      alert(`Sorry we have some problems with the form: ${err}`)
+    }
+}
 
 function LoginUser() {
-
   const navigate = useNavigate()
   const auth = useAuth()
   const [email, setEmail] = useState("usuario1@1.com")
@@ -25,18 +44,22 @@ function LoginUser() {
     }
 
 
+
   return (
     <form onSubmit={handleSubmit} style={{paddingTop:'50px'}}>
     <MainContainer>
       <WelcomeText>Welcome</WelcomeText>
+      <IntroParagraphContainer>
+        <IntroParagraph>Just for testing - NAME: usuario1 - EMAIL: usuario1@1.com  PASSWORD:111 </IntroParagraph>
+      </IntroParagraphContainer>
       <InputContainer>
         <Input type="name" placeholder="Name" name="name"  handleInputChange={(e)=>setName(e.target.value)} value={name}/>
         <Input type="text" placeholder="Email" name="email" handleInputChange={(e)=>setEmail(e.target.value)} value={email}/>
         <Input type="password" placeholder="Password" name="password"  handleInputChange={(e)=>setPassword(e.target.value)} value={password}/>
       </InputContainer>
-      <ButtonContainer>
+      {/* <ButtonContainer> */}
         <Button content="Sign Up"  />
-      </ButtonContainer>
+      {/* </ButtonContainer> */}
       <MainTitleContainer>
           <TitleContainer>H</TitleContainer>
           <MainTitle>Hotel Miranda</MainTitle>
@@ -53,7 +76,7 @@ const MainContainer = styled.div`
   padding-bottom:80px;
   align-items: center;
   flex-direction: column;
-  height: 80vh;
+  ${'' /* height: 80vh; */}
   width: 30vw;
   background: rgba(255, 255, 255, 0.15);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
@@ -99,11 +122,19 @@ const MainContainer = styled.div`
 `;
 
 const WelcomeText = styled.h2`
-  margin: 3rem 0 2rem 0;
+  margin: 3rem 0 0 0;
   color:#346f99d9;
-  padding-bottom:50px;
 `;
-
+const IntroParagraph = styled.p`
+  color: #000000;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-transform: none;
+  font-weight: 600
+`
+const IntroParagraphContainer = styled.div`
+padding: 20px 10px 50px 10px;
+`
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -113,13 +144,13 @@ const InputContainer = styled.div`
   width: 100%;
 `;
 
-const ButtonContainer = styled.div`
-  margin: 1rem 0 2rem 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+// const ButtonContainer = styled.div`
+//   margin: 1rem 0 2rem 0;
+//   width: 100%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
 const MainTitle = styled.h1`
     width:20%;
     color: var(--unnamed-color-262626);
@@ -147,6 +178,8 @@ const MainTitleContainer = styled.div`
     display: flex;
     align-items: center;
     gap: 15px;
+    padding-top: 25px;
+    padding-left: 15px;
 `
 
 export default LoginUser;
